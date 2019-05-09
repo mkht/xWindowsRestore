9c8cf8fdcb5efe734795fcef07b919cea4f39564
mkht <mkht@users.noreply.github.com>
2019-05-10 00:02:00 +0900

    Implement MaxSize property

--- DSCResources/xSystemRestore/xSystemRestore.psm1
+++ DSCResources/xSystemRestore/xSystemRestore.psm1
@@ -13,6 +13,20 @@
 #has been advised of the possibility of such damages
 #---------------------------------------------------------------------------------
 
+#Requires -Version 5.0
+using namespace System.Globalization
+
+Enum Units
+{
+    B
+    KB
+    MB
+    GB
+    TB
+    PB
+    EB
+}
+
 function Get-TargetResource
 {
     [CmdletBinding()]
@@ -73,6 +87,16 @@ function Set-TargetResource
                 {
                     Write-Verbose "Enable the System Restore feature on the '$Drive' file system drive."
                     Enable-ComputerRestore -Drive $Drive -ErrorAction Stop
+
+                    if ($PSBoundParameters.ContainsKey('Size'))
+                    {
+                        if (-not $PSBoundParameters.ContainsKey('Drive'))
+                        {
+                            throw ([InvalidParametersException]::new('Please specify the Drive property.'))
+                        }
+
+                        Set-MaximumShadowCopySize -Drive $Drive -Size $Size -ErrorAction Stop
+                    }
                 }
                 Catch
                 {
@@ -80,26 +104,25 @@ function Set-TargetResource
                     Write-Verbose $ErrorMsg
                 }
             }
-        }
-        'Absent'
-        {
-            If ($PSCmdlet.ShouldProcess("$Drive", "Disable the system restore"))
+            'Absent'
             {
-                Try
+                If ($PSCmdlet.ShouldProcess("$Drive", "Disable the system restore"))
                 {
-                    Write-Verbose "Disable the System Restore feature on the '$Drive' file system drive."
-                    Disable-ComputerRestore -Drive $Drive
-                }
-                Catch
-                {
-                    $ErrorMsg = $_.Exception.Message
-                    Write-Verbose $ErrorMsg
+                    Try
+                    {
+                        Write-Verbose "Disable the System Restore feature on the '$Drive' file system drive."
+                        Disable-ComputerRestore -Drive $Drive
+                    }
+                    Catch
+                    {
+                        $ErrorMsg = $_.Exception.Message
+                        Write-Verbose $ErrorMsg
+                    }
                 }
             }
         }
     }
 }
-
 
 function Test-TargetResource
 {
@@ -136,22 +159,34 @@ function Test-TargetResource
         }
         else
         {
-            $ActualSizeString = Parse-SizeString -Size $Size
-            $CurrentSizeInfo = Get-MaximumShadowCopySize -Drive $Drive
+            $Size = $Size.Trim()
+            $CurrentSizeInfo = Get-MaximumShadowCopySize -Drive $Drive -ErrorAction SilentlyContinue
 
-            $returnValue = $true
+            if (-not $CurrentSizeInfo)
+            {
+                return $false
+            }
+
             foreach ($info in $CurrentSizeInfo)
             {
-                if ($ActualSizeString -eq 'UNBOUNDED')
+                if ($Size -eq 'UNBOUNDED')
                 {
                     if ('UNBOUNDED' -ne $info.MaxSizeBytes)
                     {
                         return $false
                     }
                 }
-                elseif ($ActualSizeString.EndsWith('%'))
+                elseif ($Size.EndsWith('%'))
                 {
-                    if ($ActualSizeString -ne $info.MaxSizePercent)
+                    if ($Size -ne $info.MaxSizePercent)
+                    {
+                        return $false
+                    }
+                }
+                else
+                {
+                    $ConvertedSize = Convert-ByteUnit -String $Size
+                    if ($ConvertedSize -ne $info.MaxSizeBytes)
                     {
                         return $false
                     }
@@ -160,56 +195,160 @@ function Test-TargetResource
         }
     }
 
-    If ($Ensure -eq $Get.Ensure)
-    {
-        return $true
-    }
-    Else
-    {
-        return $false
-    }
+    return $true
 }
 
-function Parse-SizeString
+function Convert-ByteStringToDecimal
 {
     [CmdletBinding()]
-    [OutputType([System.String])]
-    param(
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [System.String]
-        $Size
+    [OutputType([decimal])]
+    Param(
+        [Parameter(Mandatory)]
+        [string]$String
     )
 
-    $ActualSizeString = $null
+    $kb = 1024
+    $mb = [Math]::Pow($kb, 2)
+    $gb = [Math]::Pow($kb, 3)
+    $tb = [Math]::Pow($kb, 4)
+    $pb = [Math]::Pow($kb, 5)
+    $eb = [Math]::Pow($kb, 6)
 
-    #Parse size
-    $Size = $Size.Trim()
-    if ($Size -eq 'UNBOUNDED')
+    [decimal]$Byte = 0
+    switch -Regex ($String.Trim())
     {
-        #No limit
-        $ActualSizeString = $Size.ToUpper()
-    }
-    elseif ($Size.EndsWith('%'))
-    {
-        #Percentage
-        $tempSize = $Size.Replace('%', '').Trim()
-        if ([int]::TryParse($tempSize, [ref]$null))
+        '^([0-9\.]+)(kb|k)$'
         {
-            $ActualSizeString = $tempSize + '%'
+            $Byte = [Convert]::ToDecimal($Matches[1]) * $kb
+            break
         }
 
-    }
-    elseif ($Size -match '^([0-9\.]+)(kb|mb|gb|tb|pb|eb|k|m|g|t|p|e)$')
-    {
-        #Bytes with suffix
-        $ActualSizeString = $Size
+        '^([0-9\.]+)(mb|m)$'
+        {
+            $Byte = [Convert]::ToDecimal($Matches[1]) * $mb
+            break
+        }
+
+        '^([0-9\.]+)(gb|g)$'
+        {
+            $Byte = [Convert]::ToDecimal($Matches[1]) * $gb
+            break
+        }
+
+        '^([0-9\.]+)(tb|t)$'
+        {
+            $Byte = [Convert]::ToDecimal($Matches[1]) * $tb
+            break
+        }
+
+        '^([0-9\.]+)(pb|p)$'
+        {
+            $Byte = [Convert]::ToDecimal($Matches[1]) * $pb
+            break
+        }
+
+        '^([0-9\.]+)(eb|e)$'
+        {
+            $Byte = [Convert]::ToDecimal($Matches[1]) * $eb
+            break
+        }
+
+        '^([0-9]+)$'
+        {
+            $Byte = [Convert]::ToDecimal($Matches[1])
+            break
+        }
+
+        Default
+        {
+            Write-Error -Exception ([System.ArgumentException]::new('Size is not valid format string.'))
+            return
+        }
     }
 
-    if ($null -eq $ActualSizeString)
+    $Byte
+}
+
+
+function Convert-ByteUnit
+{
+    [CmdletBinding(DefaultParameterSetName = 'Byte')]
+    [OutputType([string])]
+    Param(
+        [Parameter(Mandatory, ParameterSetName = 'String')]
+        [string]$String,
+
+        [Parameter(Mandatory, ParameterSetName = 'Decimal')]
+        [decimal]$Byte
+    )
+
+    $NumStyleFlag = [NumberStyles]::AllowDecimalPoint -bor [NumberStyles]::AllowExponent
+
+    $kb = 1024
+    $mb = [Math]::Pow($kb, 2)
+    $gb = [Math]::Pow($kb, 3)
+    $tb = [Math]::Pow($kb, 4)
+    $pb = [Math]::Pow($kb, 5)
+    $eb = [Math]::Pow($kb, 6)
+
+    if ($PSCmdlet.ParameterSetName -eq 'String')
     {
-        Write-Error -Exception ([Microsoft.PowerShell.Commands.InvalidParametersException]::new('Size is not valid format string.'))
+        $Byte = Convert-ByteStringToDecimal -String $String
     }
+
+    switch ($true)
+    {
+        { $Byte -ge $eb }
+        {
+            $target = $eb
+            $unit = [Units]::EB
+            break
+        }
+
+        { $Byte -ge $pb }
+        {
+            $target = $pb
+            $unit = [Units]::PB
+            break
+        }
+
+        { $Byte -ge $tb }
+        {
+            $target = $tb
+            $unit = [Units]::TB
+            break
+        }
+
+        { $Byte -ge $gb }
+        {
+            $target = $gb
+            $unit = [Units]::GB
+            break
+        }
+
+        { $Byte -ge $mb }
+        {
+            $target = $mb
+            $unit = [Units]::MB
+            break
+        }
+
+        { $Byte -ge $kb }
+        {
+            $target = $kb
+            $unit = [Units]::KB
+            break
+        }
+
+        Default
+        {
+            $target = 1
+            $unit = [Units]::B
+        }
+    }
+
+    $newSize = $Byte / $target
+    ([decimal]::parse($newSize.ToString('e2'), $NumStyleFlag)).toString() + ' ' + $unit.ToString()
 }
 
 
@@ -250,8 +389,8 @@ function Get-MaximumShadowCopySize
                 $vssoutput | Where-Object { $_ -match 'Maximum Shadow Copy Storage space' } | Select-Object -First 1 | ForEach-Object {
                     [pscustomobject]@{
                         Drive          = $driveItem
-                        MaxSizeBytes   = [regex]::Match($_, ':\s([^\(]+)').Groups[1].Value
-                        MaxSizePercent = [regex]::Match($_, '\(([\d\.]+)%').Groups[1].Value
+                        MaxSizeBytes   = [regex]::Match($_, ':\s([^\(]+)').Groups[1].Value.Trim()
+                        MaxSizePercent = [regex]::Match($_, '\((\d+%)').Groups[1].Value.Trim()
                     }
                 }
             }
@@ -279,7 +418,7 @@ function Get-MaximumShadowCopySize
 
 function Set-MaximumShadowCopySize
 {
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess = $true)]
     param(
         [Parameter(Mandatory = $true)]
         [System.String[]]
@@ -294,25 +433,37 @@ function Set-MaximumShadowCopySize
     Begin
     {
         $vssadmin = (Get-Command 'vssadmin.exe' -CommandType Application -ErrorAction Stop).Source
-        $ActualSizeString = Parse-SizeString -Size $Size -ErrorAction Stop
+        $Size = $Size.Trim()
+
+        $SizeOfBytes = Convert-ByteStringToDecimal -String $Size -ErrorAction SilentlyContinue
+        if ($SizeOfBytes -and ($SizeOfBytes -lt 320MB))
+        {
+            throw [System.ArgumentOutOfRangeException]::new('For byte level specification, Size must be 320MB or greater')
+        }
     }
 
     Process
     {
         foreach ($driveItem in $Drive)
         {
-            try
+            if ($PSCmdlet.ShouldProcess("'$driveItem'", "Resize maximum shadow copy storage capacity"))
             {
-                #Resize maximum shadow copy storage capacity
-                $vssoutput = & $vssadmin resize shadowstorage /On=$driveItem /For=$driveItem /MaxSize=$ActualSizeString
-                if ($LASTEXITCODE -ne 0)
+                try
                 {
-                    throw [System.InvalidOperationException]::new('Error occurs in vssadmin.exe')
+                    #Resize maximum shadow copy storage capacity
+                    Write-Verbose "Resize maximum shadow copy storage capacity on the $driveItem to $Size"
+                    $vssoutput = & $vssadmin resize shadowstorage /On=$driveItem /For=$driveItem /MaxSize=$Size
+                    if ($LASTEXITCODE -ne 0)
+                    {
+                        throw [System.InvalidOperationException]::new('Error occurs in vssadmin.exe')
+                    }
+
+                    Write-Verbose "Operation Succeeded."
                 }
-            }
-            catch
-            {
-                Write-Error -Exception $_.Exception
+                catch
+                {
+                    Write-Error -Exception $_.Exception
+                }
             }
         }
     }
